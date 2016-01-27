8a0e2886ee3e586648af7bc68cdf2d81a1aab5ac
Huaren YU <codegeek@qq.com>
2016-01-27 15:59:10 +0800

    Mappper bind error

--- src/main/java/com/lwjb/tour/controllers/IndexController.java
+++ src/main/java/com/lwjb/tour/controllers/IndexController.java
@@ -1,20 +1,29 @@
 package com.lwjb.tour.controllers;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.lwjb.tour.daos.ItemDao;
+import com.lwjb.tour.models.Item;
+import com.lwjb.tour.services.ItemService;
+
 @Controller
 @RequestMapping(path = { "/" })
 public class IndexController {
-	
+	@Autowired
+	private ItemService itemService;
 	private final Logger logger = LoggerFactory.getLogger(IndexController.class);
 	
 	@RequestMapping(path = { "" }, method = { RequestMethod.GET })
 	public String index(Model model) {
+		List<Item> items = itemService.findAll();
 		model.addAttribute("title", "首页");
 		return "index";
 	}
---
+++ src/main/java/com/lwjb/tour/services/ItemService.java
@@ -0,0 +1,36 @@
+package com.lwjb.tour.services;
+
+import java.util.List;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authc.credential.PasswordService;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lwjb.tour.daos.ItemDao;
+import com.lwjb.tour.daos.UserDao;
+import com.lwjb.tour.exceptions.UserExistException;
+import com.lwjb.tour.forms.LoginForm;
+import com.lwjb.tour.forms.RegisterForm;
+import com.lwjb.tour.models.Item;
+import com.lwjb.tour.models.User;
+
+@Service
+@Transactional
+public class ItemService {
+	
+	@Autowired
+	private ItemDao itemDao;
+	
+	private static final Logger logger = LoggerFactory.getLogger(ItemService.class);
+	
+	public List<Item> findAll() {
+		return itemDao.findAll();
+	}
+	
+}
